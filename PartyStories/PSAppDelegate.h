81fabfadd4a09bc315e87b96c884ebf98d025e22
Matt Ao <mattyayoh@gmail.com>
2014-02-24 20:09:30 -0500

    Initial Commit

---
+++ PartyStories/PSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PSAppDelegate.h
+//  PartyStories
+//
+//  Created by Matthew Ao on 2/24/14.
+//  Copyright (c) 2014 Matthew Ao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
